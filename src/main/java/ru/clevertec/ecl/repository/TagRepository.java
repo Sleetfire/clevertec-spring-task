3f2f90a87459eb689e9228fe22d5889a41b7663a
SleetFire <barkovskiy2000@gmail.com>
2023-04-04 09:05:38 +0300

    feat: add hibernate annotations to the entities

--- src/main/java/ru/clevertec/ecl/repository/TagRepository.java
+++ src/main/java/ru/clevertec/ecl/repository/TagRepository.java
@@ -1,110 +1,99 @@
 package ru.clevertec.ecl.repository;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 import ru.clevertec.ecl.repository.api.ITagRepository;
-import ru.clevertec.ecl.repository.mappers.TagSQLMapper;
-import ru.clevertec.ecl.repository.util.QueryUtil;
+import ru.clevertec.ecl.repository.util.HibernateUtil;
 import ru.clevertec.ecl.repository.entity.TagEntity;
 
-import javax.sql.DataSource;
-import java.util.Collections;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaDelete;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.CriteriaUpdate;
+import javax.persistence.criteria.Root;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public class TagRepository implements ITagRepository {
 
-    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-    private static final Logger logger = LogManager.getLogger(TagRepository.class);
-
-    public TagRepository(@Qualifier("dataSource") DataSource dataSource) {
-        this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    @Override
+    public Long create(TagEntity entity) {
+        return HibernateUtil.saveEntity(entity);
     }
 
     @Override
-    public long create(TagEntity tag) {
-        MapSqlParameterSource paramMap = new MapSqlParameterSource();
-        paramMap.addValue("name", tag.getName());
-        String sqlInsertQuery = "insert into ecl.tags (name) values (:name)";
-        long tagId = QueryUtil.executeQuery(sqlInsertQuery, paramMap, this.namedParameterJdbcTemplate);
-        logger.info("Created tag with id " + tagId);
-        return tagId;
-    }
-
-    @Override
-    public Optional<TagEntity> getById(long id) {
-        String sqlSelectQuery = "select * from ecl.tags where id = :id";
-        MapSqlParameterSource paramMap = new MapSqlParameterSource();
-        paramMap.addValue("id", id);
-        return Optional.ofNullable(this.getEntity(sqlSelectQuery, paramMap));
-    }
-
-    @Override
-    public Optional<TagEntity> getByName(String name) {
-        String sqlSelectQuery = "select * from ecl.tags where name = :name";
-        MapSqlParameterSource paramMap = new MapSqlParameterSource();
-        paramMap.addValue("name", name);
-        return Optional.ofNullable(this.getEntity(sqlSelectQuery, paramMap));
+    public Optional<TagEntity> getById(Long id) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<TagEntity> selectCriteriaQuery = cb.createQuery(TagEntity.class);
+        Root<TagEntity> root = selectCriteriaQuery.from(TagEntity.class);
+        selectCriteriaQuery.select(root).where(
+                cb.equal(root.get("id"), id)
+        );
+        return HibernateUtil.getSingleEntity(session, selectCriteriaQuery);
     }
 
     @Override
     public List<TagEntity> getAll() {
-        String sqlSelectQuery = "select * from ecl.tags";
-        try {
-            return this.namedParameterJdbcTemplate.query(sqlSelectQuery, new TagSQLMapper()).get(0);
-        } catch (DataAccessException e) {
-            logger.error(e.getMessage());
-            return Collections.emptyList();
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<TagEntity> selectCriteriaQuery = cb.createQuery(TagEntity.class);
+        Root<TagEntity> root = selectCriteriaQuery.from(TagEntity.class);
+        selectCriteriaQuery.select(root);
+        return HibernateUtil.getAllEntities(session, selectCriteriaQuery);
+    }
+
+    @Override
+    public Long update(Long id, TagEntity updated) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaUpdate<TagEntity> criteriaUpdate = cb.createCriteriaUpdate(TagEntity.class);
+        Root<TagEntity> root = criteriaUpdate.from(TagEntity.class);
+
+        String tagName = updated.getName();
+        if (tagName != null) {
+            criteriaUpdate.set("name", tagName);
         }
+        criteriaUpdate.where(
+                cb.equal(root.get("id"), id)
+        );
+
+        HibernateUtil.updateEntity(session, criteriaUpdate);
+        return id;
     }
 
     @Override
-    public long update(long id, TagEntity updated) {
-        String sqlUpdateQuery = "update ecl.tags set name = :updated_name where id = :id";
-        MapSqlParameterSource paramMap = new MapSqlParameterSource();
-        paramMap.addValue("id", id);
-        paramMap.addValue("updated_name", updated.getName());
-        long tagId = QueryUtil.executeQuery(sqlUpdateQuery, paramMap, this.namedParameterJdbcTemplate);
-        logger.info("Updated tag with id" + tagId);
-        return tagId;
+    public void delete(Long id) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaDelete<TagEntity> criteriaDelete = cb.createCriteriaDelete(TagEntity.class);
+        Root<TagEntity> root = criteriaDelete.from(TagEntity.class);
+        criteriaDelete.where(
+                cb.equal(root.get("id"), id)
+        );
+        HibernateUtil.deleteEntity(session, criteriaDelete);
     }
 
     @Override
-    public void delete(long id) {
-        String sqlDeleteQueryTemporalTable = "delete from ecl.gift_certificates_tags where tag_id = :id";
-        String sqlDeleteQuery = "delete from ecl.tags where id = :id";
-        MapSqlParameterSource paramMap = new MapSqlParameterSource();
-        paramMap.addValue("id", id);
-        QueryUtil.executeQuery(sqlDeleteQueryTemporalTable, paramMap, this.namedParameterJdbcTemplate);
-        QueryUtil.executeQuery(sqlDeleteQuery, paramMap, this.namedParameterJdbcTemplate);
-        logger.info("Deleted tag with id " + id);
+    public Optional<TagEntity> getByName(String name) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<TagEntity> tagCriteriaQuery = cb.createQuery(TagEntity.class);
+        Root<TagEntity> root = tagCriteriaQuery.from(TagEntity.class);
+        tagCriteriaQuery.select(root).where(
+                cb.equal(root.get("name"), name)
+        );
+        return HibernateUtil.getSingleEntity(session, tagCriteriaQuery);
     }
 
     @Override
     public void delete() {
-        String sqlDeleteQuery = "delete from ecl.gift_certificates_tags;\ndelete from ecl.tags;";
-        QueryUtil.executeQuery(sqlDeleteQuery, new MapSqlParameterSource(), this.namedParameterJdbcTemplate);
-    }
-
-    private TagEntity getEntity(String sqlSelectQuery, MapSqlParameterSource paramMap) {
-        try {
-            List<TagEntity> content = this.namedParameterJdbcTemplate.queryForObject(sqlSelectQuery, paramMap, new TagSQLMapper());
-            if (content != null && content.size() == 1) {
-                return content.get(0);
-            } else {
-                return null;
-            }
-        } catch (DataAccessException e) {
-            logger.error(e.getMessage());
-            System.out.println(e.getMessage());
-            return null;
-        }
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaDelete<TagEntity> criteriaDelete = cb.createCriteriaDelete(TagEntity.class);
+        criteriaDelete.from(TagEntity.class);
+        HibernateUtil.deleteEntity(session, criteriaDelete);
     }
 
 }
--- src/main/java/ru/clevertec/ecl/repository/entity/TagEntity.java
+++ src/main/java/ru/clevertec/ecl/repository/entity/TagEntity.java
@@ -5,11 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tags", schema = "ecl")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class TagEntity {
-    private long id;
+public class TagEntity implements BaseEntity<Long> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
 }
