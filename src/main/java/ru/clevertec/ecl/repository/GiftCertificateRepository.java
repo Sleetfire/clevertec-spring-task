208cecb54993aa27b58072f49081ef20a6a24d3c
SleetFire <barkovskiy2000@gmail.com>
2023-04-09 23:39:55 +0300

    feat: add jpa repositories

--- src/main/java/ru/clevertec/ecl/repository/GiftCertificateRepository.java
+++ src/main/java/ru/clevertec/ecl/repository/GiftCertificateRepository.java
@@ -1,72 +1,19 @@
 package ru.clevertec.ecl.repository;
 
-import ru.clevertec.ecl.dto.GiftCertificateFilter;
-import ru.clevertec.ecl.repository.entity.GiftCertificateEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.clevertec.ecl.repository.entity.GiftCertificate;
 
 import java.util.List;
-import java.util.Optional;
 
 /**
  * Interface {@code IGiftCertificateRepository} defines methods for CRUD operations with GiftCertificateEntity
  *
  * @version 0.1
  */
-public interface GiftCertificateRepository extends CrudRepository<GiftCertificateEntity> {
+@Repository
+public interface GiftCertificateRepository extends JpaRepository<GiftCertificate, Long> {
 
-    /**
-     * Method for creating GiftCertificateEntity
-     *
-     * @param entity entity to creating
-     * @return created GiftCertificateEntity's id
-     */
-    @Override
-    long create(GiftCertificateEntity entity);
+    List<GiftCertificate> findAllByNameLikeAndDescriptionLikeAndTagsContains(String name, String description, String tagName);
 
-    /**
-     * Method for getting GiftCertificateEntity by id
-     *
-     * @param id entity's id
-     * @return GiftCertificateEntity in Optional wrapper
-     */
-    @Override
-    Optional<GiftCertificateEntity> findById(long id);
-
-    /**
-     * Method for getting all GiftCertificateEntities
-     *
-     * @return list of GiftCertificateEntities
-     */
-    @Override
-    List<GiftCertificateEntity> findAll();
-
-    /**
-     * Method for updating entity
-     *
-     * @param id            entity's id
-     * @param updatedEntity entity with updated fields
-     * @return updated GiftCertificateEntity's id
-     */
-    @Override
-    long update(long id, GiftCertificateEntity updatedEntity);
-
-    /**
-     * Method for deleting GiftCertificateEntity by id
-     *
-     * @param id entity's id
-     */
-    @Override
-    void delete(long id);
-
-    /**
-     * Method for getting all GiftCertificateEntities with filter
-     *
-     * @param filter filter with search fields
-     * @return list of GiftCertificateEntities
-     */
-    List<GiftCertificateEntity> getAll(GiftCertificateFilter filter);
-
-    /**
-     * Method for deleting all GiftCertificateEntities
-     */
-    void delete();
 }
--- src/main/java/ru/clevertec/ecl/repository/TagRepository.java
+++ src/main/java/ru/clevertec/ecl/repository/TagRepository.java
@@ -1,64 +1,17 @@
 package ru.clevertec.ecl.repository;
 
-import ru.clevertec.ecl.repository.entity.TagEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.clevertec.ecl.repository.entity.Tag;
 
-import java.util.List;
 import java.util.Optional;
 
 /**
  * Interface {@code ITagRepository} defines methods for CRUD operations with TagEntity
  */
-public interface TagRepository extends CrudRepository<TagEntity> {
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long> {
 
-    /**
-     * Method for creating TagEntity
-     * @param entity entity to creating
-     * @return created TagEntity's id
-     */
-    @Override
-    long create(TagEntity entity);
-
-    /**
-     * Method for getting TagEntity by id
-     * @param id entity's id
-     * @return TagEntity in Optional wrapper
-     */
-    @Override
-    Optional<TagEntity> findById(long id);
-
-    /**
-     * Method for getting all TagEntities
-     * @return list of TagEntities
-     */
-    @Override
-    List<TagEntity> findAll();
-
-    /**
-     * Method for updating entity
-     * @param id            entity's id
-     * @param updated entity with updated fields
-     * @return updated TagEntity's id
-     */
-    @Override
-    long update(long id, TagEntity updated);
-
-    /**
-     * Method for deleting TagEntity by id
-     * @param id entity's id
-     */
-    @Override
-    void delete(long id);
-
-    /**
-     * Method for getting TagEntity by name
-     * @param name TagEntity's name
-     * @return TagEntity in Optional wrapper
-     */
-    Optional<TagEntity> findByName(String name);
-
-    /**
-     * Method for deleting all TagEntities
-     */
-    void delete();
+    Optional<Tag> findByName(String name);
 
 }
