12eeafde869383368c5b55389e37bd9abf799726
SleetFire <barkovskiy2000@gmail.com>
2023-04-09 23:38:08 +0300

    feat: add hibernate to the entities

--- src/main/java/ru/clevertec/ecl/repository/entity/Tag.java
+++ src/main/java/ru/clevertec/ecl/repository/entity/Tag.java
@@ -1,16 +1,20 @@
 package ru.clevertec.ecl.repository.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "tags", schema = "ecl")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class TagEntity {
-
-    private long id;
+public class Tag implements BaseEntity<Long> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
 }
