d562e9cdc6dcf7ce9e1dc3ab36fc1bfc80f6fb13
SleetFire <barkovskiy2000@gmail.com>
2023-04-10 14:28:03 +0300

    feat: add filtered searching

--- src/main/java/ru/clevertec/ecl/repository/GiftCertificateRepository.java
+++ src/main/java/ru/clevertec/ecl/repository/GiftCertificateRepository.java
@@ -1,19 +1,24 @@
 package ru.clevertec.ecl.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.clevertec.ecl.repository.entity.GiftCertificate;
 
 import java.util.List;
 
 /**
- * Interface {@code IGiftCertificateRepository} defines methods for CRUD operations with GiftCertificateEntity
+ * Interface {@code GiftCertificateRepository} defines methods for CRUD operations with GiftCertificate
  *
- * @version 0.1
+ * @version 2.1
  */
 @Repository
 public interface GiftCertificateRepository extends JpaRepository<GiftCertificate, Long> {
 
-    List<GiftCertificate> findAllByNameLikeAndDescriptionLikeAndTagsContains(String name, String description, String tagName);
+    @Query("select g from GiftCertificate g left join g.tags t where (g.name like ?1 or g.description like ?2) or t.name = ?3")
+    List<GiftCertificate> findAllFiltered(String name, String description, String tagName, Sort sort);
 
 }
