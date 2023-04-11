918181c25568cac0f86921983bdeb5fbc3dc674b
SleetFire <barkovskiy2000@gmail.com>
2023-04-11 10:16:52 +0300

    fix: fix order mapper

--- src/main/java/ru/clevertec/ecl/mapper/OrderMapper.java
+++ src/main/java/ru/clevertec/ecl/mapper/OrderMapper.java
@@ -2,6 +2,7 @@ package ru.clevertec.ecl.mapper;
 
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.clevertec.ecl.dto.OrderDto;
 import ru.clevertec.ecl.repository.entity.Order;
 
@@ -10,12 +11,16 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
 
+    @Mapping(source = "order.giftCertificates", target = "certificates")
     OrderDto toDto(Order order);
 
+    @Mapping(source = "orderDto.certificates", target = "giftCertificates")
     Order toEntity(OrderDto orderDto);
 
+    @Mapping(source = "order.giftCertificates", target = "certificates")
     List<OrderDto> toDto(List<Order> orders);
 
+    @Mapping(source = "orderDto.certificates", target = "giftCertificates")
     List<Order> toEntity(List<OrderDto> orders);
 
 }
