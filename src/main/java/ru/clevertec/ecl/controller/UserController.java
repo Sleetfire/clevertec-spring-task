14d1bdbbb25b4fb89304d45f4e3cb421253dcfb9
SleetFire <barkovskiy2000@gmail.com>
2023-04-11 10:17:27 +0300

    refactor: change directory

--- src/main/java/ru/clevertec/ecl/controller/UserController.java
+++ src/main/java/ru/clevertec/ecl/controller/UserController.java
@@ -1,9 +1,10 @@
-package ru.clevertec.ecl.controller.advice;
+package ru.clevertec.ecl.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.clevertec.ecl.dto.CreateOrderDto;
@@ -24,14 +25,14 @@ public class UserController {
     @PostMapping(value = "/",
             produces = {MediaType.APPLICATION_JSON_VALUE},
             consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<UserDto> create(UserDto userDto) {
+    public ResponseEntity<UserDto> create(@RequestBody UserDto userDto) {
         return new ResponseEntity<>(userService.create(userDto), HttpStatus.CREATED);
     }
 
     @PostMapping(value = "/order",
             produces = {MediaType.APPLICATION_JSON_VALUE},
             consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<OrderDto> makeOrder(CreateOrderDto createOrderDto) {
+    public ResponseEntity<OrderDto> makeOrder(@RequestBody CreateOrderDto createOrderDto) {
         return new ResponseEntity<>(userService.makeOrder(createOrderDto), HttpStatus.CREATED);
     }
 
