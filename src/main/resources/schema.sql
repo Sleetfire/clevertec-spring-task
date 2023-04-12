ff9f8d3e6eba0520596ad0d5b4cc9891d2936f69
SleetFire <barkovskiy2000@gmail.com>
2023-04-12 16:47:11 +0300

    fix: fix sql schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,7 +2,7 @@ CREATE SCHEMA IF NOT EXISTS ecl;
 
 CREATE TABLE IF NOT EXISTS ecl.gift_certificates (
     id bigserial PRIMARY KEY,
-    name character varying (20) NOT NULL,
+    name character varying (20) NOT NULL UNIQUE,
     description character varying (50) NOT NULL,
     price numeric NOT NULL,
     duration bigint NOT NULL,
@@ -17,8 +17,8 @@ CREATE TABLE IF NOT EXISTS ecl.tags (
 
 CREATE TABLE IF NOT EXISTS ecl.gift_certificates_tags (
     id bigserial PRIMARY KEY,
-    gift_certificate_id bigserial,
-    tag_id bigserial,
+    gift_certificate_id bigserial NOT NULL,
+    tag_id bigserial NOT NULL,
     CONSTRAINT gift_certificates_fk FOREIGN KEY (gift_certificate_id) REFERENCES ecl.gift_certificates(id),
     CONSTRAINT tags_fk FOREIGN KEY (tag_id) REFERENCES ecl.tags(id)
 );
@@ -45,44 +45,6 @@ CREATE TABLE IF NOT EXISTS ecl.gift_certificates_orders (
      CONSTRAINT orders_fk FOREIGN KEY (order_id) REFERENCES ecl.orders(id)
     );
 
-CREATE OR REPLACE FUNCTION pattern_find_certificates(argument CHARACTER) RETURNS TABLE(id BIGINT, name CHARACTER VARYING, description CHARACTER VARYING,
-                                                                                       price DECIMAL, duration BIGINT, create_date CHARACTER VARYING,
-                                                                                       last_update_date CHARACTER VARYING, tag_id BIGINT,
-                                                                                       tag_name CHARACTER VARYING)
-    COST 10
-    ROWS 100
-    LANGUAGE plpgsql
-as $$
-BEGIN
-    RETURN QUERY
-        SELECT gift_certificates.id, gift_certificates.name, ecl.gift_certificates.description, ecl.gift_certificates.price,
-               ecl.gift_certificates.duration, ecl.gift_certificates.create_date, ecl.gift_certificates.last_update_date,
-               tags.id as tag_id, tags.name as tag_name
-        FROM ecl.gift_certificates
-                 LEFT JOIN ecl.gift_certificates_tags ON ecl.gift_certificates.id = ecl.gift_certificates_tags.gift_certificate_id
-                 LEFT JOIN ecl.tags ON ecl.gift_certificates_tags.tag_id = ecl.tags.id
-        WHERE (gift_certificates.name LIKE (argument) OR ecl.gift_certificates.description LIKE (argument));
-END
-$$;
-
-CREATE OR REPLACE FUNCTION find_certificates() RETURNS TABLE(id BIGINT, name CHARACTER VARYING, description CHARACTER VARYING,
-                                                             price DECIMAL, duration BIGINT, create_date CHARACTER VARYING,
-                                                             last_update_date CHARACTER VARYING, tag_id BIGINT,
-                                                             tag_name CHARACTER VARYING)
-    COST 10
-    ROWS 100
-    LANGUAGE plpgsql
-as $$
-BEGIN
-    RETURN QUERY
-        SELECT gift_certificates.id, gift_certificates.name, ecl.gift_certificates.description, ecl.gift_certificates.price,
-               ecl.gift_certificates.duration, ecl.gift_certificates.create_date, ecl.gift_certificates.last_update_date,
-               tags.id as tag_id, tags.name as tag_name
-        FROM ecl.gift_certificates
-                 LEFT JOIN ecl.gift_certificates_tags ON ecl.gift_certificates.id = ecl.gift_certificates_tags.gift_certificate_id
-                 LEFT JOIN ecl.tags ON ecl.gift_certificates_tags.tag_id = ecl.tags.id;
-END
-$$;
 
 
 
