e1e7633cfc7453c38b4b88fc2985f9ca4a56b9f0
SleetFire <barkovskiy2000@gmail.com>
2023-04-27 13:07:23 +0300

    fix: add java doc

--- src/main/java/ru/clevertec/ecl/repository/util/EntityUtil.java
+++ src/main/java/ru/clevertec/ecl/repository/util/EntityUtil.java
@@ -9,35 +9,45 @@ import javax.persistence.criteria.*;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+
 @UtilityClass
 public class EntityUtil {
 
     public static Map<String, Object> getMap(GiftCertificateEntity giftCertificate) {
         Map<String, Object> values = new HashMap<>();
 
-        if (giftCertificate.getName() != null) {
+        if (nonNull(giftCertificate.getName())) {
             values.put("name", giftCertificate.getName());
         }
 
-        if (giftCertificate.getDescription() != null) {
+        if (nonNull(giftCertificate.getDescription())) {
             values.put("description", giftCertificate.getDescription());
         }
 
-        if (giftCertificate.getPrice() != null) {
+        if (nonNull(giftCertificate.getPrice())) {
             values.put("price", giftCertificate.getPrice());
         }
 
-        if (giftCertificate.getDuration() != null) {
+        if (nonNull(giftCertificate.getDuration())) {
             values.put("duration", giftCertificate.getDuration());
         }
 
-        if (giftCertificate.getLastUpdateDate() != null) {
+        if (nonNull(giftCertificate.getLastUpdateDate())) {
             values.put("last_update_date", DateUtil.getCurrentDateISO8601());
         }
 
         return values;
     }
 
+    /**
+     * Method for modifying query by filter params
+     * @param filter filter with searching params
+     * @param cb query builder
+     * @param root query root
+     * @param criteriaQuery query
+     */
     public static void filterQuery(GiftCertificateFilter filter, CriteriaBuilder cb, Root<GiftCertificateEntity> root,
                                    CriteriaQuery<GiftCertificateEntity> criteriaQuery) {
         String tagName = filter.getTagName();
@@ -49,57 +59,67 @@ public class EntityUtil {
         Predicate fieldPartPredicate = null;
         Predicate tagPredicate;
 
-        if (fieldPart != null) {
+        //search by part of the field
+        if (nonNull(fieldPart)) {
             fieldPart = "%" + fieldPart + "%";
             fieldPartPredicate = cb.or(cb.like(root.get("name"), fieldPart), cb.like(root.get("description"), fieldPart));
             lastPredicate = cb.and(fieldPartPredicate);
         }
 
-        if (tagName != null) {
+        // search by tag name
+        if (nonNull(tagName)) {
             Join join = root.join("tags");
             tagPredicate = cb.equal(join.get("name"), tagName);
-            if (fieldPartPredicate != null) {
+            if (nonNull(fieldPartPredicate)) {
                 lastPredicate = cb.and(fieldPartPredicate, tagPredicate);
             } else {
                 lastPredicate = cb.and(tagPredicate);
             }
         }
 
-        if (lastPredicate != null) {
+        if (nonNull(lastPredicate)) {
             criteriaQuery.where(lastPredicate);
         } else {
             criteriaQuery.select(root);
         }
 
-        if ((sortName != null && sortName.contains("asc")) && (sortDate != null && sortDate.contains("asc"))) {
+        // sort by certificate and create date asc
+        if ((nonNull(sortName) && sortName.contains("asc")) && (nonNull(sortDate) && sortDate.contains("asc"))) {
             criteriaQuery.orderBy(cb.asc(root.get("name")), cb.asc(root.get("createDate")));
         }
 
-        if ((sortName != null && sortName.contains("desc")) && (sortDate != null && sortDate.contains("desc"))) {
+        // sort by certificate and create date desc
+        if ((nonNull(sortName) && sortName.contains("desc")) && (nonNull(sortDate) && sortDate.contains("desc"))) {
             criteriaQuery.orderBy(cb.desc(root.get("name")), cb.desc(root.get("createDate")));
         }
 
-        if ((sortName != null && sortName.contains("asc")) && (sortDate != null && sortDate.contains("desc"))) {
+        // sort by certificate asc and create date desc
+        if ((nonNull(sortName) && sortName.contains("asc")) && (nonNull(sortDate) && sortDate.contains("desc"))) {
             criteriaQuery.orderBy(cb.asc(root.get("name")), cb.desc(root.get("createDate")));
         }
 
-        if ((sortName != null && sortName.contains("desc")) && (sortDate != null && sortDate.contains("asc"))) {
+        // sort by certificate desc and create date asc
+        if ((nonNull(sortName) && sortName.contains("desc")) && (nonNull(sortDate) && sortDate.contains("asc"))) {
             criteriaQuery.orderBy(cb.desc(root.get("name")), cb.asc(root.get("createDate")));
         }
 
-        if ((sortName != null && sortName.contains("asc")) && (sortDate == null)) {
+        // sort by certificate asc
+        if ((nonNull(sortName) && sortName.contains("asc")) && isNull(sortDate)) {
             criteriaQuery.orderBy(cb.asc(root.get("name")));
         }
 
-        if ((sortName != null && sortName.contains("desc")) && (sortDate == null)) {
+        // sort by certificate desc
+        if ((nonNull(sortName) && sortName.contains("desc")) && isNull(sortDate)) {
             criteriaQuery.orderBy(cb.desc(root.get("name")));
         }
 
-        if ((sortDate != null && sortDate.contains("asc")) && (sortName == null)) {
+        // sort by create date asc
+        if ((nonNull(sortDate) && sortDate.contains("asc")) && isNull(sortName)) {
             criteriaQuery.orderBy(cb.asc(root.get("createDate")));
         }
 
-        if ((sortDate != null && sortDate.contains("desc")) && (sortName == null)) {
+        // sort by create date desc
+        if ((nonNull(sortDate) && sortDate.contains("desc")) && isNull(sortName)) {
             criteriaQuery.orderBy(cb.desc(root.get("createDate")));
         }
 
